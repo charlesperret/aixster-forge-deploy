d62d881dc665067c3320f8139055e27f7d3ff347
charlesperret <charlesperret1@gmail.com>
2025-12-12 19:17:33 +0800

    Update src/pages/_app.tsx via SEO-Forge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Pillar 4: Mobile-first viewport meta tag */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
